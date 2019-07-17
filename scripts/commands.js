0c704761facf9e972aa332b6b6eb643325ae0909
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-07-17 18:39:50 +0300

    added healing and health reduction functions...

--- scripts/commands.js
+++ scripts/commands.js
@@ -2,42 +2,11 @@ const yargs = require('yargs');
 const runner = require('./runner');
 
 yargs.command({
-  command: 'location',
-  describe: 'prints out the current player location and the list of exits',
-  handler: function () {
-    runner.location();
-  }
-});
-
-yargs.command({
-  command: 'items',
-  describe: 'prints out the list of items in the backpack',
-  handler: function () {
-    runner.items();
-  }
-});
-
-yargs.command({
-  command: 'pickup',
-  describe: 'you will pickup the item and execute the items command',
-  builder:{
-    item: {
-      describe:'Enum of items: [sword, bomb, banana, key_blue, key_red, key_green, med_kit]',
-      demandOption: true,
-      type: 'string'
-    }
-  },
-  handler: function (argv) {
-    runner.pickup(argv.item);
-  }
-});
-
-yargs.command({
   command: 'drop',
   describe: 'you will drop the item and execute the items command',
   builder:{
     item: {
-      describe:'Enum of items: [sword, bomb, banana, key_blue, key_red, key_green, med_kit]',
+      describe:'enum of items: [sword, bomb, banana, key_blue, key_red, key_green, med_kit]',
       demandOption: true,
       type: 'string'
     }
@@ -52,12 +21,12 @@ yargs.command({
   describe: 'player will move to the exit that connects the current and selected rooms and will execute the location command',
   builder:{
     room: {
-      describe:'Available: room_0 to room_23 inclusive',
+      describe:'available: room_0 to room_23 inclusive',
       demandOption: true,
       type: 'string'
     },
     key: {
-      describe:'An item to enter the locked/blocked room',
+      describe:'an item needed to enter the locked/blocked room',
       demandOption: false,
       type: 'string'
     }
@@ -66,6 +35,46 @@ yargs.command({
     runner.goto(argv.room, argv.key);
   }
 });
+
+yargs.command({
+  command: 'heal',
+  describe: 'heals your character if you have a med_kit',
+  handler: function () {
+    runner.heal();
+  }
+});
+
+yargs.command({
+  command: 'items',
+  describe: 'prints out the list of items in the backpack',
+  handler: function () {
+    runner.items();
+  }
+});
+
+yargs.command({
+  command: 'location',
+  describe: 'prints out the current player location and the list of exits',
+  handler: function () {
+    runner.location();
+  }
+});
+
+yargs.command({
+  command: 'pickup',
+  describe: 'you will pickup the item and execute the items command',
+  builder:{
+    item: {
+      describe:'enum of items: [sword, bomb, banana, key_blue, key_red, key_green, med_kit]',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    runner.pickup(argv.item);
+  }
+});
+
 
 yargs.command({
   command: 'quite',
--- scripts/runner.js
+++ scripts/runner.js
@@ -1,19 +1,71 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
+const drop = (itemType) => {
+  let runner = load('runner'),
+    removable = runner.backpack.items.find((e)=>e.type === itemType),
+    index = runner.backpack.items.indexOf(removable);
+
+  if (index >-1) {
+    runner.backpack.items.splice(index,1);
+    runner.backpack.curr_weight -= removable.weight;
+    save(runner, 'runner');
+  } else {
+    return console.log(chalk.red(`No such item in the backpack...`))
+  }
+  console.log(chalk.green(`What is in my backpack (after drop)? \r\n ${JSON.stringify(runner.backpack)}`));
+};
+
+const goto = (room, key) => {
+  let maze = load('maze'),
+    runner = load('runner'),
+    curr_room = maze.find((e)=>e.name === runner.current_room),
+    exit_match = curr_room.exits.includes(room),
+    blocked_exit = curr_room.blocked_exits.find((b)=>b.includes(`${room}--`)),
+    blocker;
+
+  if (blocked_exit) {
+    blocker = blocked_exit.split('--')[1];
+  }
+  if (exit_match) {
+    if (!blocker) {
+      moveAlong(runner, room)
+    }
+    attemptUnlock(blocker, key, runner, room);
+  } else {
+    return console.log(chalk.red(`Cannot go to room ${room}.`))
+  }
+};
+
+const heal = () => {
+  let runner = load('runner'),
+    med_kit = runner.backpack.items.find((i) => i.type === 'med_kit'),
+    index = runner.backpack.items.indexOf(med_kit);
+
+  if (index > -1) {
+    runner.backpack.items.splice(index, 1);
+    runner.backpack.curr_weight -= med_kit.weight;
+    runner.health +=10;
+    console.log(chalk.green.underline('Using med_kit... health up by 10 pts.'));
+    save(runner, 'runner');
+  } else {
+    console.log(chalk.red('Could not find med_kit...'));
+  }
+};
+
+const items = () => {
+  let {backpack} = load('runner');
+  console.log(chalk.green(`What is in my backpack? \r\n ${JSON.stringify(backpack.items)}`));
+};
+
 const location = () => {
   let maze = load('maze'),
       runner = load('runner'),
-      item = getItemInRoom(maze, runner); //# kind-of-ugly...
+      item = getItemInRoom(maze, runner);
 
   console.log(chalk.yellow(`Currently you are in room: ${runner.current_room} \r\n` +
     'Available item: \r\n' +
     `${JSON.stringify(item)}`));
-};
-
-const items = () => {
- let {backpack} = load('runner');
-  console.log(chalk.green(`What is in my backpack? \r\n ${JSON.stringify(backpack.items)}`));
 };
 
 const pickup = (itemType) => {
@@ -36,74 +88,34 @@ const pickup = (itemType) => {
   console.log(chalk.green(`What is in my backpack (after pickup)? \r\n ${JSON.stringify(runner.backpack.items)}`));
 };
 
-const drop = (itemType) => {
-  let runner = load('runner'),
-  removable = runner.backpack.items.find((e)=>e.type === itemType),
-  index = runner.backpack.items.indexOf(removable);
-
-  if (index >-1) {
-    runner.backpack.items.splice(index,1);
-    runner.backpack.curr_weight -= removable.weight;
-    save(runner, 'runner');
-  } else {
-    return console.log(chalk.red(`No such item in the backpack...`))
-  }
-  console.log(chalk.green(`What is in my backpack (after drop)? \r\n ${JSON.stringify(runner.backpack)}`));
-};
-
-
-const goto = (room, key) => {
-  let maze = load('maze'),
-    runner = load('runner'),
-    curr_room = maze.find((e)=>e.name === runner.current_room),
-    exit_match = curr_room.exits.includes(room),
-    blocked_exit = curr_room.blocked_exits.find((b)=>b.includes(`${room}--`)),
-    blocker;
-
-  if (blocked_exit) {
-    blocker = blocked_exit.split('--')[1];
-  }
-
-  console.log(exit_match);
-
- if (exit_match) {
-    if(blocked_exit && !key){
-      console.log(chalk.red(`Cannot go to room ${room}. Exit is blocked with ${blocker}! Lookup
-      your backpack to see if you have a solution.`));
-    } else if(key === blocker){
-      let match =runner.backpack.items.find((item)=>item.type===key),
-          index = runner.backpack.items.indexOf(match);
-      if (match) {
-        runner.backpack.items.splice(index,1);
-        runner.backpack.curr_weight -= match.weight;
-        runner.current_room = room;
-        //TODO if necessary also update the blocked exits by removing the one at hand so it remains open at all times
-        save(runner, 'runner');
-        console.log(chalk.green(`Going to room ${room} now...`));
-        console.log(location());
-        winner(room);
-      }
-    }
-     if (!blocker) {
-       runner.current_room = room;
-       save(runner, 'runner');
-       console.log(chalk.green(`Going to room ${room} now...`));
-       console.log(location());
-       winner(room)
-     }
- } else {
-   return console.log(chalk.red(`Cannot go to room ${room}.`))
- }
-};
 
 const quite = () => {
   console.log(chalk.cyan('Shutting down... Have a nice day and come back tomorrow!'));
 };
 
-const winner = (room) => {
-  if (room === "room_17"){
-     return console.log(chalk.brightGreen('Hurra! You won the maze run... What a game it was, huh?'))
+//helper functions
+
+const attemptUnlock = (blocker, key, runner, room) => {
+  if(blocker && !key){
+    console.log(chalk.red(`Cannot go to room ${room}. Exit is blocked with ${blocker}! Lookup
+      your backpack to see if you have a solution.`));
+  } else if(key === blocker){
+    let match = runner.backpack.items.find((item)=>item.type===key),
+      index = runner.backpack.items.indexOf(match);
+    if (match) {
+      runner.backpack.items.splice(index,1);
+      runner.backpack.curr_weight -= match.weight;
+      runner.current_room = room;
+      reduceHealth(runner, room);
+      moveAlong(runner, room);
+    }
+  }else {
+    return console.log(chalk.red(`No such item in the backpack...`))
   }
+};
+
+const getItemInRoom = (maze, runner) =>{
+  return maze.filter((item)=>item.name===runner.current_room)[0].item
 };
 
 const load = (filename) => {
@@ -117,12 +129,36 @@ const load = (filename) => {
   }
 };
 
+const moveAlong = (runner, room) => {
+  reduceHealth(runner, room);
+  runner.current_room = room;
+  save(runner, 'runner');
+  console.log(chalk.green(`Going to room ${room} now...`));
+  console.log(location());
+  winner(room);
+};
+
+const reduceHealth = (runner, room)=> {
+  let diff = room.split('_')[1]-runner.current_room.split('_')[1];
+  
+  if (diff>0 && diff <=3 || diff === -1) {
+    runner.health -=10;
+  } else if(diff>4 && diff<8) {
+    runner.health-=20;
+  } else {
+    return console.log(chalk.red(`No clipping in space allowed...`))
+  }
+};
+
 const save = (document, filename) => {
   const entry = JSON.stringify(document);
   fs.writeFileSync(`JSON/${filename}.json`, entry);
 };
 
-const getItemInRoom = (maze, runner) =>{
-  return maze.filter((item)=>item.name===runner.current_room)[0].item
+const winner = (room) => {
+  if (room === "room_17"){
+    return console.log(chalk.green('Hurra! You won the maze run... What a game it was, huh?'))
+  }
 };
-module.exports = {location, goto, items, pickup, drop, quite};
+
+module.exports = {drop, goto, heal, items, location, pickup, quite};
